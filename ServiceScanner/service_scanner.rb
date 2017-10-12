1160dbbb806573801b7c75208affdd5da1982ac2
Germán Castro <germancastromontes@gmail.com>
2017-10-12 16:22:40 +0200

    Añadido gemfile

--- ServiceScanner/service_scanner.rb
+++ ServiceScanner/service_scanner.rb
@@ -1,6 +1,8 @@
 require_relative 'scanner.rb'
 require_relative 'parser.rb'
 
-Scanner.scan
+ip = ARGV[0]
+ip ='127.0.0.1' if ARGV.empty?
+#Scanner.scan(ip)
 parseador = Parser.new()
-parseador.print
+parseador.print_output
